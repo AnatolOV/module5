41150b4050077aa26bbd63caea22b63e675c77f6
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-08-06 21:22:51 +0300

    task1

---
+++ module_5_1.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+a = House(2, 9)
+a.go_to(8)
